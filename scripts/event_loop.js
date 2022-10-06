c7bc84cafa288e69eb6c20b6d1cea547f2938539
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-10-06 17:20:59 -0300

    Intervalos de Tiempo

--- scripts/event_loop.js
+++ scripts/event_loop.js
@@ -1,20 +1,32 @@
-function message(text){
-    console.log(text)
-    console.log('funcion message');
+const 
+    timer = d.querySelector('#time p');
+    btnStart = d.querySelector('#time button'),
+    btnEnd = d.querySelector('#time button:last-child');
+let s = 0;
+let m = 0;
+let h = 0;
+
+let startTime;
+
+function addTime (){
+    s < 59 ? s++ : s = 0;
+    m < 59 && s == 0 ? m++ : s == 0 ? m = 0 : m;
+    m == 0 && s == 0 ? h++ : h
+    timer.innerHTML = `
+        ${h}:${m < 10 ? '0' + m : m}:${s < 10 ? '0' + s : s}`
 }
 
-let myMessage = function(text, callback){
-    console.log(text)
-    console.log('funcion myMessage');
-    setTimeout( () => callback(text), 5000)
+function restartTime(){
+    alert(`Felicitaciones: tu tiempo fue ${timer.innerHTML}`);
+    timer.innerHTML = `HH:mm:ss`;
+    s = 0; m = 0; h = 0;
 }
 
-const newMessage = (text) => {
-    console.log(text)
-    console.log('funcion newMessage');
-}
+btnStart.addEventListener('click', () => {
+    startTime = setInterval(addTime, 10)
+} );
 
-// Orden de ejecucion
-setTimeout(() => message('primera'), 3000 )
-myMessage('segunda', message);
-newMessage('ultima');
+btnEnd.addEventListener('click', () => {
+    clearInterval(startTime);
+    restartTime();
+}   )
