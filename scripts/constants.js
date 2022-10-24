276f5aa6ed79585db8e460c253974212881465dc
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-10-24 14:57:57 -0300

    Creacion de Modulo de Constantes

---
+++ scripts/constants.js
@@ -0,0 +1,14 @@
+export const d = document,
+    searchInput = d.querySelector('#searchInput'),
+    suggestion = d.querySelector('#suggestion'),
+    searchButton = d.querySelector('#searchButton'),
+    searchForm = d.querySelector('#searchForm'),
+    main = d.querySelector('main'),
+    links = [
+        'users',
+        'contact',
+        'posts',
+        'comments',
+        'todos',
+        'albums'
+    ];
