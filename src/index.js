f70635b1b586ab06870dc6451ac60dd0b94e3238
Cristian Racedo <CristianDRacedo@hotmail.com>
2022-10-31 17:56:54 -0300

    Carga y lectura de Datos iDB

--- src/index.js
+++ src/index.js
@@ -13,8 +13,21 @@ if (indexedDB){
         console.log("Cambios realizados: ", e.target.result)
         let db = e.target.result;
         // Creacion de Tabla (Objeto) y clave principal (Indice)
-        db.createObjectStore('students', { 
+        let keys = db.createObjectStore('students', { 
             keyPath: 'email'
         } );
+        keys.transaction.oncomplete = (e) => {
+            // Acceso para lectoescritura
+            const access = db.transaction('students', 'readwrite')
+            // Rectificacion de la tabla a modificar
+            const students = access.objectStore('students');
+            // Cargar los datos en la coleccion
+            students.add({
+                email: 'cristiandracedo@hotmail.com', 
+                name: 'cristian'
+            })
+            // Leer los datos de un indice especifico
+            students.get( 'cristiandracedo@hotmail.com')
+        }
     }
 }
